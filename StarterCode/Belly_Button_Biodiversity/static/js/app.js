c51e74b3ae64b8ecc4dc452d8691ab5b624878f4
melodyquintero <cheungwansum@hotmail.com>
2018-08-05 23:55:50 -0500

    draft

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,27 +3,65 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  const metaDatatURL = `/metadata/${sample}`;
+  d3.json(metaDatatURL).then(function(data) {
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    var panel = d3.select(`#sample-metadata`);
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
     // Use `Object.entries` to add each key and value pair to the panel
+    Object.entries(data).map(function([key,value]){
+      panel.append('P').text(`${key}: ${value}`);
+    });
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  buildGauge(data.WFREQ);
+  })
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  const sampleDatatURL = `/samples/${sample}`;
+
+  d3.json(sampleDatatURL).then(function(data) {
 
     // @TODO: Build a Bubble Chart using the sample data
 
+    var BubbleTrace = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      mode='markers',
+      text: data.otu_labels,
+      marker: {
+        color: data.otu_ids,
+        size: data.sample_values
+      }
+    };
+
+    Plotly.newPlot('bubble', [BubbleTrace]);
+
+
     // @TODO: Build a Pie Chart
+    var top_sample_values = data.sample_values.slice(0,10);
+    var top_otu_ids = data.otu_ids.slice(0,10);
+    var top_otu_labels = data.otu_labels.slice(0,10);
+
+
+    var PieTrace = {
+      "labels": top_otu_ids,
+      "values": top_sample_values,
+      "hovertext": top_otu_labels,
+      "type": "pie",
+    };
+
+    
+    Plotly.newPlot('pie', [PieTrace]);
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+  })
 }
 
 function init() {
